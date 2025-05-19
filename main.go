4bb78b53164feb6e3559ab73f07a5eaa84ddee7c
sikiru <sikirurazak1@gmail.com>
2025-05-19 02:44:47 +0100

    feat: Integrate PostgreSQL database and environment variables
    This commit integrates a PostgreSQL database connection and utilizes environment variables for configuration.
    
    - Added `godotenv` and `lib/pq` dependencies to `go.mod` and `go.sum`.
    - Implemented loading environment variables from a `.env` file using `godotenv`.
    - Established a connection to a PostgreSQL database using the `DB_URL` environment variable.
    - Configured the server to listen on a port specified by the `PORT` environment variable.
    - Added error handling for database connection and environment variable loading.
    - Added executable permissions to tmp/main

--- main.go
+++ main.go
@@ -1,22 +1,57 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func main(){
+	err := godotenv.Load();
+	if err != nil {
+		log.Fatal("Error loading .env files")
+	}
+	dbStr := os.Getenv("DB_URL");
+	portString := os.Getenv("PORT");
+
+	db, err := sql.Open("postgres",dbStr);
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close();
+
+	if err = db.Ping(); err != nil {
+		log.Fatal((err))
+	}
+
 	r := chi.NewRouter();
 	r.Use(middleware.Logger);
 	r.Use(middleware.Recoverer);
 	r.Use(cors.AllowAll().Handler);//tempoarily allow all origins
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request){
-		w.Write([]byte("sharp Go server is active!!!"))
+		w.Write([]byte("Go server is active!!!"))
 	})
 
-	http.ListenAndServe(":8000",r)
+	srv := http.Server{
+		Addr: ":" + portString,
+		Handler: r,
+	}
+
+	func()  {
+		log.Printf("server is running on port %s",portString);
+		err = srv.ListenAndServe();
+		if err != nil {
+			log.Fatal(err)
+		}	
+	}()
 }
